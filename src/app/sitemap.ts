9d6761cbd0a932503c445d173603ef19733f7e32
okashadev <ukashahashim231@gmail.com>
2026-08-30 15:50:54 -0700

    changes

---
+++ src/app/sitemap.ts
@@ -0,0 +1,21 @@
+import { X } from "lucide-react";
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = "https://inkly-ecru.vercel.app";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/authors`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+  ];
+}
